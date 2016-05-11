29c4592ef6e29bc7c009cdcc0b5adc69a9e08ed6
Alison <alisonhmullin@gmail.com>
2016-05-11 15:46:24 -0400

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,8 +1,3 @@
-
-# require "pry"
-
-nums=[1,2,3]
-
 def my_select(collection)
   new_array = []
   i = 0 
@@ -20,27 +15,3 @@ end
 
 
 
-# my_select([1,2,3,4,5]) 
-
-
-# binding.pry
-
-
-# def my_select(collection)
-#   new_array = []
-#   i = 0 
-#     while i < collection.length
-#       if yield collection[i] == true 
-#         new_array.push(collection[i])
-#         i++
-#       else
-#         i++
-#       end
-#     end
-#   new_array
-# end
-
-# nums = [1, 2, 3, 4]
-# my_select(nums) do |num|
-#       num.even?
-# end
